99de0d90178850daa55d675b2ffd50c6d4676ae1
pracan <58209642+pracan@users.noreply.github.com>
2023-03-16 22:30:47 +0100

    OM self linkage done

--- src/components/Traveling_Salesman_Starcitizen.vue
+++ src/components/Traveling_Salesman_Starcitizen.vue
@@ -18,21 +18,31 @@ import { onMounted, onUnmounted } from 'vue';
 
 const jsonData = {
   name: 'ArcCorp',
+  type: 'Planet',
   children: [
     {
-      name: 'Jump',
+      name: 'OM',
       children: [
-        { name: 'OM-1' },
-        { name: 'OM-2' },
-        { name: 'OM-3' },
-        { name: 'OM-4' },
-        { name: 'OM-5' },
-        { name: 'OM-6' },
+        { name: 'OM-1', no_qt: 'OM-1' },
+        { name: 'OM-2', no_qt: 'OM-2' },
+        { name: 'OM-3', no_qt: 'OM-4' },
+        { name: 'OM-4', no_qt: 'OM-3' },
+        { name: 'OM-5', no_qt: 'OM-6' },
+        { name: 'OM-6', no_qt: 'OM-5' },
       ],
     },
     {
       name: 'Space',
-      children: [{ name: 'Baijin Point' }, { name: 'Comm Array ST3-90' }],
+      children: [
+        {
+          name: 'Baijin Point',
+          qt_point: [{ name: 'OM-2' }, { name: 'OM-4' }, { name: 'OM-6' }],
+        },
+        {
+          name: 'Comm Array ST3-90',
+          qt_point: [{ name: 'OM-4' }, { name: 'OM-5' }],
+        },
+      ],
     },
     {
       name: 'Self',
@@ -43,21 +53,45 @@ const jsonData = {
         { name: 'Area11' },
         { name: 'Area17' },
         { name: 'Area20' },
+        { name: 'The Sky Scrapper' },
       ],
     },
     {
       name: 'Lyria',
+      type: 'Moon',
       children: [
         {
+          name: 'OM',
+          children: [
+            { name: 'OM-1', no_qt: 'OM-1' },
+            { name: 'OM-2', no_qt: 'OM-2' },
+            { name: 'OM-3', no_qt: 'OM-4' },
+            { name: 'OM-4', no_qt: 'OM-3' },
+            { name: 'OM-5', no_qt: 'OM-6' },
+            { name: 'OM-6', no_qt: 'OM-5' },
+          ],
+        },
+        {
           name: 'space',
-          children: [{ name: 'Comm Array ST3-18' }],
+          children: [
+            {
+              name: 'Comm Array ST3-18',
+              qt_point: [
+                { name: 'OM-1' },
+                { name: 'OM-2' },
+                { name: 'OM-3' },
+                { name: 'OM-4' },
+                { name: 'OM-6' },
+              ],
+            },
+          ],
         },
         {
           name: 'no_qt',
           children: [
             {
               name: 'Paradise Cove',
-              children: [{ name: "Teddy's Playhouse" }],
+              near: [{ name: "Teddy's Playhouse" }],
             },
           ],
         },
@@ -83,17 +117,39 @@ const jsonData = {
     },
     {
       name: 'Wala',
+      type: 'Moon',
       children: [
         {
+          name: 'OM',
+          children: [
+            { name: 'OM-1', no_qt: 'OM-1' },
+            { name: 'OM-2', no_qt: 'OM-2' },
+            { name: 'OM-3', no_qt: 'OM-4' },
+            { name: 'OM-4', no_qt: 'OM-3' },
+            { name: 'OM-5', no_qt: 'OM-6' },
+            { name: 'OM-6', no_qt: 'OM-5' },
+          ],
+        },
+        {
           name: 'space',
-          children: [{ name: 'Comm Array ST3-35' }],
+          children: [
+            {
+              name: 'Comm Array ST3-35',
+              qt_point: [
+                { name: 'OM-1' },
+                { name: 'OM-2' },
+                { name: 'OM-4' },
+                { name: 'OM-5' },
+              ],
+            },
+          ],
         },
         {
           name: 'no_qt',
           children: [
             {
               name: 'Good Times Temple',
-              children: [{ name: 'Shady Glen Farms' }],
+              near: [{ name: 'Shady Glen Farms' }],
             },
           ],
         },
@@ -117,11 +173,11 @@ onMounted(() => {
   const container = document.getElementById('sigma-container');
   //dataJson.data
   const graphData = transformData(jsonData);
-  console.log(graphData);
+  // console.log(graphData);
   graph.import(graphData);
-  console.log(graph);
+  // console.log(graph);
   graph.nodes().forEach((node, i) => {
-    console.log(graph.getNodeAttributes(node));
+    // console.log(graph.getNodeAttributes(node));
     graph.setNodeAttribute(node, 'size', 5);
     // graph.setNodeAttribute(node, 'type', 'image');
     // graph.setNodeAttribute(
@@ -131,39 +187,39 @@ onMounted(() => {
     // );
     let bob = graph.getNodeAttribute(node, 'label');
     let bob2 = String(bob);
-    console.log('Value :', bob);
-    console.log(bob.toString());
-    console.log(bob2 == 'ArcCorp' ? 'True' : 'False');
+    // console.log('Value :', bob);
+    // console.log(bob.toString());
+    // console.log(bob2 == 'ArcCorp' ? 'True' : 'False');
     switch (bob2) {
       case 'space':
         graph.setNodeAttribute(node, 'color', '#0066cc');
-        console.log('True : space');
+        // console.log('True : space');
         break;
       case 'no_qt':
         graph.setNodeAttribute(node, 'color', '#ff9900');
-        console.log('True : no_qt');
+        // console.log('True : no_qt');
         break;
       case 'ArcCorp':
         graph.setNodeAttribute(node, 'color', '#00cc66');
         //graph.setNodeAttribute(node, 'label', node.name);
         graph.setNodeAttribute(node, 'size', '15');
-        console.log('True : ArcCorp');
+        // console.log('True : ArcCorp');
         break;
       case 'Lyria':
         graph.setNodeAttribute(node, 'color', '#cc0066');
         //graph.setNodeAttribute(node, 'label', node.name);
         graph.setNodeAttribute(node, 'size', '10');
-        console.log('True : Lyria');
+        // console.log('True : Lyria');
         break;
       case 'Wala':
         graph.setNodeAttribute(node, 'color', '#9900cc');
         //graph.setNodeAttribute(node, 'label', node.name);
-        console.log('True : Wala');
+        // console.log('True : Wala');
         graph.setNodeAttribute(node, 'size', '10');
         break;
       default:
         graph.setNodeAttribute(node, 'color', '#cccccc');
-        console.log('Default');
+        // console.log('Default');
         break;
     }
   });
@@ -214,6 +270,7 @@ function transformData(data) {
       attributes: {
         label: obj.name,
         parent: parent ? `${parent}.${obj.name}` : obj.name,
+        sc_type: obj['type'] || '',
       },
       label: obj.name,
     };
@@ -227,67 +284,184 @@ function transformData(data) {
     }
   }
 
-  function addJumpEdges(nodes, edges) {
-    const arcCorpNode = nodes.find(
-      (node) => node.attributes.label === 'ArcCorp'
-    );
-    console.log('arcCorpNode :', arcCorpNode);
-    console.log('arcCorpNode.atr.parent :', arcCorpNode.attributes.parent);
-    const arcCorpChildrenLabels = edges
-      .filter((edge) => edge.source === arcCorpNode.key)
-      .map(
-        (edge) =>
-          nodes.find((node) => node.key === edge.target).attributes.label
-      );
-    console.log('arcCorpChildrenLabels :', arcCorpChildrenLabels);
-    const jumpNode = nodes.find(
-      (node) =>
-        node.attributes.parent ===
-        String(arcCorpNode.attributes.parent) + '.' + 'Jump'
-    );
-    console.log('jumpNode :', jumpNode);
+  // function addJumpEdges(nodes, edges) {
+  //   const arcCorpNode = nodes.find(
+  //     (node) => node.attributes.label === 'ArcCorp'
+  //   );
+  //   console.log('arcCorpNode :', arcCorpNode);
+  //   console.log('arcCorpNode.atr.parent :', arcCorpNode.attributes.parent);
+  //   const arcCorpChildrenLabels = edges
+  //     .filter((edge) => edge.source === arcCorpNode.key)
+  //     .map(
+  //       (edge) =>
+  //         nodes.find((node) => node.key === edge.target).attributes.label
+  //     );
+  //   console.log('arcCorpChildrenLabels :', arcCorpChildrenLabels);
+  //   const jumpNode = nodes.find(
+  //     (node) =>
+  //       node.attributes.parent ===
+  //       String(arcCorpNode.attributes.parent) + '.' + 'OM'
+  //   );
+  //   console.log('jumpNode :', jumpNode);
 
-    if (jumpNode) {
-      const jumpNodeChildrenLabels = edges
-        .filter((edge) => edge.source === jumpNode.key)
+  //   if (jumpNode) {
+  //     const jumpNodeChildrenLabels = edges
+  //       .filter((edge) => edge.source === jumpNode.key)
+  //       .map(
+  //         (edge) =>
+  //           nodes.find((node) => node.key === edge.target).attributes.label
+  //       );
+  //     console.log('jumpNodeChildrenLabels :', jumpNodeChildrenLabels);
+
+  //     for (const jumpchild of jumpNodeChildrenLabels) {
+  //       console.log('jumpchild :', jumpchild);
+  //       const jumpchildNode = nodes.find(
+  //         (node) => node.attributes.label === jumpchild
+  //       );
+  //       console.log('jumpchildNode :', jumpchildNode);
+  //       for (const arcchild of arcCorpChildrenLabels) {
+  //         console.log('arcchild :', arcchild);
+  //         const arcchildNode = nodes.find(
+  //           (node) => node.attributes.label === arcchild
+  //         );
+  //         console.log('arcchildNode :', arcchildNode);
+  //         if (
+  //           arcchildNode.attributes.label !== 'Self' &&
+  //           arcchildNode.attributes.label !== 'Jump'
+  //         ) {
+  //           edges.push({
+  //             key: count++,
+  //             source: jumpchildNode.key,
+  //             target: arcchildNode.key,
+  //           });
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return { edges };
+  // }
+
+  function addOMJumpEdges(nodes, edges) {
+    const planets = nodes.filter(
+      (node) => node.attributes.sc_type === 'Planet'
+    );
+    // console.log('planets :', planets);
+    for (const planet of planets) {
+      const planetChildrenLabels = edges
+        .filter((edge) => edge.source === planet.key)
         .map(
           (edge) =>
             nodes.find((node) => node.key === edge.target).attributes.label
         );
-      console.log('jumpNodeChildrenLabels :', jumpNodeChildrenLabels);
+      // console.log('planetChildrenLabels :', planetChildrenLabels);
 
-      for (const jumpchild of jumpNodeChildrenLabels) {
-        console.log('jumpchild :', jumpchild);
-        const jumpchildNode = nodes.find(
-          (node) => node.attributes.label === jumpchild
-        );
-        console.log('jumpchildNode :', jumpchildNode);
-        for (const arcchild of arcCorpChildrenLabels) {
-          console.log('arcchild :', arcchild);
-          const arcchildNode = nodes.find(
-            (node) => node.attributes.label === arcchild
+      const jumpNode = nodes.find(
+        (node) =>
+          node.attributes.parent === `${planet.attributes.parent}` + `.` + `OM`
+      );
+      // console.log('jumpNode :', jumpNode);
+      if (jumpNode) {
+        const jumpNodeChildrenLabels = edges
+          .filter((edge) => edge.source === jumpNode.key)
+          .map(
+            (edge) =>
+              nodes.find((node) => node.key === edge.target).attributes.label
           );
-          console.log('arcchildNode :', arcchildNode);
+        // console.log('jumpNodeChildrenLabels :', jumpNodeChildrenLabels);
+        for (const jumpchild of jumpNodeChildrenLabels) {
+          const jumpchildNode = nodes.find(
+            (node) => node.attributes.label === jumpchild
+          );
+          // console.log('jumpchild :', jumpchild);
+          // console.log('jumpchildNode :', jumpchildNode);
+          for (const planetChild of planetChildrenLabels) {
+            const planetChildNode = nodes.find(
+              (node) => node.attributes.label === planetChild
+            );
+            // console.log('planetChild :', planetChild);
+            // console.log('planetChildNode :', planetChildNode);
+            if (
+              planetChildNode.attributes.label !== 'Self' &&
+              planetChildNode.attributes.label !== 'Space' &&
+              planetChildNode.attributes.label !== 'OM'
+            ) {
+              edges.push(
+                {
+                  key: count++,
+                  source: jumpchildNode.key,
+                  target: planetChildNode.key,
+                },
+                {
+                  key: count++,
+                  source: planetChildNode.key,
+                  target: jumpchildNode.key,
+                }
+              );
+            }
+          }
+        }
+      }
+    }
+
+    return { edges };
+  }
+
+  function addOMSelfEdges(nodes, edges) {
+    const OMParents = nodes.filter((node) => node.attributes.label === 'OM');
+    // console.log('OMParents :', OMParents);
+    for (const OMParent of OMParents) {
+      const OMChildrenLabels = edges
+        .filter((edge) => edge.source === OMParent.key)
+        .map((edge) => nodes.find((node) => node.key === edge.target));
+      // console.log('OMChildrenLabels :', OMChildrenLabels);
+
+      for (const OMChildrenNode of OMChildrenLabels) {
+        // console.log('OMChildrenNode.id', OMChildrenNode.id);
+        const id_temp = String(`${OMChildrenNode.id}`).slice(-1);
+        // console.log('id_temp :', id_temp);
+        let exclude_id = String(`${OMChildrenNode.id}`).slice(0, -1);
+        if (parseInt(id_temp) % 2 == 0) {
+          exclude_id += `${parseInt(id_temp) - 1}`;
+          // console.log('0 : exclude_id :', exclude_id);
+        } else {
+          exclude_id += `${parseInt(id_temp) + 1}`;
+          // console.log('1 : exclude_id :', exclude_id);
+        }
+        for (const OMChildBrotherNode of OMChildrenLabels) {
+          // console.log(
+          //   'Exclude id : |',
+          //   `${exclude_id}`,
+          //   '| OMChildBrotherNode.attributes.id : |',
+          //   `${OMChildBrotherNode.id}`,
+          //   '| Different ?',
+          //   `${OMChildBrotherNode.id}` !== `${exclude_id}` ? 'True' : 'False',
+          //   '| Allowed ?',
+          //   OMChildBrotherNode.id !== OMChildrenNode.id ? 'True' : 'False'
+          // );
           if (
-            arcchildNode.attributes.label !== 'Self' &&
-            arcchildNode.attributes.label !== 'Jump'
+            `${OMChildBrotherNode.id}` !== `${exclude_id}` &&
+            OMChildBrotherNode.id !== OMChildrenNode.id
           ) {
             edges.push({
               key: count++,
-              source: jumpchildNode.key,
-              target: arcchildNode.key,
+              source: OMChildrenNode.key,
+              target: OMChildBrotherNode.key,
             });
           }
         }
       }
     }
+
     return { edges };
   }
 
   createNodes(data, null);
-  console.log(edges);
-  edges = addJumpEdges(nodes, edges).edges;
-  console.log(edges);
+  // console.log(edges);
+  edges = addOMJumpEdges(nodes, edges).edges;
+  console.log('nodes :', nodes);
+  // console.log('edges :', edges);
+  edges = addOMSelfEdges(nodes, edges).edges;
+  console.log('edges :', edges);
 
   return { nodes, edges };
 }
